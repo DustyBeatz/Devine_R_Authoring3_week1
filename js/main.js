59f8903e37c666ff2a09064cc5b060a3f0642ee0
Reece Devine <reece.devine97@gmail.com>
2020-10-12 14:34:34 -0300

    adding dynamic content to our view

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 //import your package here
-import Person from "./modules/DataModules.js";
-import NavSystem from "./modules/TheNavSystem";
+import Team from "./modules/DataModules.js";
+//import NavSystem from "./modules/TheNavSystem";
 
-(() =>{
+(() => {
     //stub *just a place for none compenent stuff
-    console.log('loaded');
+    let userSection = document.querySelector(".user-section"),
+        userTemplate = document.querySelector("#profs-template").content;
 
-    console.log(Person);
-    console.log(NavSystem);
+
+    // debugger;
+
+    //select our users elements and load the content
+
+    function handleDataSet(data) {
+        for (let user in data) {
+
+            // make a copy of our template here and then
+            // populate the children (text elements) with 
+            // the static data from Team object
+            let currentUser = userTemplate.cloneNode(true),
+            currentUserText = currentUser.querySelector('.user').children;
+
+            currentUserText[1].textContent = data[user].name;
+            currentUserText[2].textContent = data[user].role;
+            currentUserText[3].textContent = data[user].nickname;
+
+            userSection.appendChild(currentUser);
+        }
+    }
+
+    handleDataSet(Team);    
+
 })();
--- js/modules/DataModules.js
+++ js/modules/DataModules.js
@@ -1,5 +1,27 @@
 export default {
-    name: "Reece", 
-    role: "Student",
-    nickname: "Reece"
+
+Justin: {
+    name: "Justin",
+    role: "Coordinator",
+    nickname: "Big J"
+},
+
+John :{
+    name: "John",
+    role: "super chill prof",
+    nickname: "just john"
+},
+
+Joe: {
+    name: "Joe",
+    role: "prof",
+    nickname: "teddybear"
+},
+
+Pan: {
+    name: "Pan",
+    role: "prof",
+    nickname: "code ninja"
+}
+
 }
