b15b4fc449c644e6676ba21191c89059404988f2
Reece Devine <reece.devine97@gmail.com>
2020-10-25 22:35:00 -0300

    Done with writing fetch

--- js/main.js
+++ js/main.js
@@ -1,70 +1,36 @@
 (() => {
-    //XHTTP is the old triend-and-true way of doing AJAX - still relevant, but there are better options
+    //start a fetch call
+    fetch('./DataSet.json')
+        .then(res => res.json()) //parse the JSON (translate) back to plain Javascript
+        .then(data => {
+            //this is our data (DataSet.json)
+            //converted  to a plain JavaScript
+            handleDataSet(data);
+        })
+    .catch((error) => console.log(error));
 
-//create an instance of the AJAX object
-    let myReq = new XMLHttpRequest;
 
-    // add an event handler so that we can track the stages of the request and respond accordingly
-    myReq.addEventListener("readystatechange", handleRequest);
-
-    // get the request ready to go / configure it with method and resource request
-    myReq.open('GET', '../DataSet.json');
-
-    //send the request off to the server
-    myReq.send();
-
-    //this is a passive listener function - it gets invoked for every stage of the AJAX request. When the request is done and the data payload is returned from the server it passes that data to the handleDataSet function
-
-    function handleRequest() {
-        debugger;
-        if (myReq.readyState === XMLHttpRequest.DONE) {
-            debugger;
-            // check status here and proceed
-            if (myReq.status === 200) {
-                // 200 means done and dusted, ready to go with the dataset!
-                handleDataSet(myReq.responseText);
-
-            } else {
-                // probably got some kind of error code, so handle that 
-                // a 404, 500 etc... can render appropriate error messages here
-                console.error(`${myReq.status} : something done broke, son`);
-            }
-        } else {
-            // request isn't ready yet, keep waiting...
-            console.log(`Request state: ${myReq.readyState}. Still processing...`);
-        }
-
-    }
-
-    debugger;
-
-    // fetch(url)
-    // .then(response =>response.json())
-    // .then( data => console.log(data))
-    // .catch((err) => console.error(err));
-
-    //select our users elements and load the content
-
+// this is the data payload from our ajax request, parses it
     function handleDataSet(data) {
 
-        let myData = JSON.parse(data),
-        userSection = document.querySelector(".user-section"),
+        let userSection = document.querySelector(".user-section"),
         userTemplate = document.querySelector("#profs-template").content;
 
 
-        for (let user in myData) {
+        for (let user in data) {
 
             debugger;
 
             // make a copy of our template here and then
             // populate the children (text elements) with 
-            // the static data from myData object
+            // the static data from data object
             let currentUser = userTemplate.cloneNode(true),
             currentUserText = currentUser.querySelector('.user').children;
 
-            currentUserText[1].textContent = myData[user].name;
-            currentUserText[2].textContent = myData[user].role;
-            currentUserText[3].textContent = myData[user].nickname;
+            currentUserText[1].src = `images/${data[user].avatar}.jpg`;
+            currentUserText[2].textContent = data[user].name;
+            currentUserText[3].textContent = data[user].role;
+            currentUserText[4].textContent = data[user].nickname;
 
             userSection.appendChild(currentUser);
         }
